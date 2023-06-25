8bc4e15356e2c770a1d605dcbd381e6e9b1db005
Neelkamal <neelkamalsinghthegisb@gmail.com>
2023-06-25 13:37:08 +0530

    Constructors destructors done

--- student_example.cpp
+++ student_example.cpp
@@ -49,42 +49,48 @@ public:
         }
         age = n;
     }
+
+    ~Student()
+    {
+        cout << "Destructor called !!" << endl;
+    }
 };
 
 int main()
 {
+    /*
     // Create objects statically
-    // Student s1;
-    // Student s2;
+    Student s1;
+    Student s2;
 
-    // Student s3, s4, s5;
-    // s1.age = 24;
-    // s1.rollNumber = 101;
+    Student s3, s4, s5;
+    s1.age = 24;
+    s1.rollNumber = 101;
 
     // Create objects dynamically
-    // s1.display();
+    s1.display();
 
-    // cout << "S1 age : " << s1.getAge() << '\n';
-    // cout << "S1 Roll number : " << s1.rollNumber << '\n';
+    cout << "S1 age : " << s1.getAge() << '\n';
+    cout << "S1 Roll number : " << s1.rollNumber << '\n';
 
-    // Student *s6 = new Student;
-    // (*s6).age = 23;
-    // (*s6).age = 104;
+    Student *s6 = new Student;
+    (*s6).age = 23;
+    (*s6).age = 104;
     // Or we can do the same thing using ->
 
-    // s6->age = 23;
-    // s6->rollNumber = 104;
+    s6->age = 23;
+    s6->rollNumber = 104;
 
-    // s3.setAge(12);
-    // s3.rollNumber = 105;
+    s3.setAge(12);
+    s3.rollNumber = 105;
 
-    // Student *s7 = new Student;
-    // s7->setAge(17);
-    // s7->rollNumber = 106;
+    Student *s7 = new Student;
+    s7->setAge(17);
+    s7->rollNumber = 106;
 
-    // s3.display();
-    // s7->display();
-
+    s3.display();
+    s7->display();
+*/
     Student s1;
     s1.display();
 
@@ -97,4 +103,5 @@ int main()
 
     Student *s5 = new Student(20, 1002);
     s5->display();
+    delete (s5); // Explicitly called Destructor for dynamically created object
 }
