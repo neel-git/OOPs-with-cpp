385c793bffe067214361c59836ccee9bda197f7f
Neelkamal <neelkamalsinghthegisb@gmail.com>
2023-06-25 14:33:07 +0530

    addition function added

---
+++ Fraction_class.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Fraction
+{
+private:
+    int numerator;
+    int denominator;
+
+public:
+    Fraction(int numerator, int denominator)
+    {
+        this->numerator = numerator;
+        this->denominator = denominator;
+    }
+
+    void print()
+    {
+        cout << this->numerator << " / " << denominator << '\n'; // Here this keyword isn't necessary because yha function ke argument me koi dusra numerator ni h to wo implicitally object ka hi num/den access krega
+    }
+
+    void simplify()
+    {
+        int gcd = 1;
+        int j = min(this->numerator, this->denominator);
+        for (int i = 1; i <= j; i++)
+        {
+            if (this->numerator % i == 0 && this->denominator % i == 0)
+            {
+                gcd = i;
+            }
+        }
+        this->numerator /= gcd;
+        this->denominator /= gcd;
+    }
+
+    void add(Fraction f2)
+    {
+        int lcm = denominator * f2.denominator;
+        int x = lcm / denominator;
+        int y = lcm / f2.denominator;
+
+        int num = x * numerator + (y * f2.numerator);
+
+        numerator = num;
+        denominator = lcm;
+
+        simplify();
+    }
+};
+
+int main()
+{
+    Fraction f1(10, 2), f2(15, 4);
+
+    f1.add(f2);
+    f1.print();
+    f2.print();
+}
